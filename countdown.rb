30d2604cbe134360418a4293b8d819998ff41449
Yingqi Chen <chanwk18@hotmail.com>
2019-03-22 20:21:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(count)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(count)
+  until count==0
+    puts "#{count} SECOND(S)!"
+    count-=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+  
